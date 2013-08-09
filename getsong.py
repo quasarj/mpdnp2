18e3c2728109987bbac6a83cd20381265703f10c
Quasar Jarosz <quasar@ja.rosz.org>
2013-08-09 00:24:08 -0500

    initial commit

---
+++ getsong.py
@@ -0,0 +1,22 @@
+"""
+    Module to retrieve a song from google/youtube.
+    Depends on google module, from: https://github.com/MarioVilas/google
+"""
+from google import search
+
+
+def get_song_url(song):
+    try:
+        return search('site:youtube.com {}'.format(song), 
+                      stop=1, 
+                      pause=0).next()  
+    except:
+        return "No youtube video found :("
+    
+
+if __name__ == '__main__':
+    #song = "Modest Mouse - Float On"
+    #song = "Coldplay - Yellow"
+    #song = "Test of Time - Warez Song"
+    song = "fkgherickj592udjgkaskj98"
+    print get_song_url(song)
